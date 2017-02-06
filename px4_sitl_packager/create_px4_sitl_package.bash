3612edce81b9d4a84e34e74391c96c7fb587b182
Julian Oes <julian@oes.ch>
2017-02-06 11:28:06 +0100

    Improve px4_sitl_package script, add README for it

--- px4_sitl_packager/create_px4_sitl_package.bash
+++ px4_sitl_packager/create_px4_sitl_package.bash
@@ -4,8 +4,21 @@
 #
 # @author: Julian Oes <julian@oes.ch>
 
-# This script needs to run inside the firmware directory.
-if [ ! -f ./Firmware.sublime-project ]; then
+# Stop on error
+set -e
+
+
+argc=$#
+if [ $argc -ne 1 ];
+then
+	echo "Usage: create_px4_sitl_package.bash px4_firmware_path"
+	exit 1
+fi
+
+firmwaredir=$1
+
+# Check the Firmware path
+if [ ! -f "$firmwaredir/Firmware.sublime-project" ]; then
     echo "Please run inside Firmware directory"
     exit 1
 fi
@@ -14,11 +27,19 @@ fi
 version=`git describe --always --tags`
 
 tmpdir="/tmp/sitl"
-builddir="build_posix_sitl_ekf2"
+builddir="$firmwaredir/build_posix_sitl_ekf2"
 
 if [ ! -f "$builddir/src/firmware/posix/px4" ]; then
     echo "Please run 'make posix_sitl_ekf2 gazebo_typhoon_h480' manually first"
     exit 1
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    lib_ending="dylib"
+    os_string="macOS"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    lib_ending="so"
+    os_string="Linux"
 fi
 
 
@@ -31,41 +52,35 @@ mkdir -p $tmpdir/build_gazebo
 
 #cp -r $builddir/* $tmpdir
 cp $builddir/src/firmware/posix/px4 $tmpdir/src/firmware/posix/
-cp $builddir/build_gazebo/libgazebo_geotagged_images_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/libgazebo_lidar_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/libgazebo_opticalFlow_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/libLiftDragPlugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/libmav_msgs.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_controller_interface.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_gimbal_controller_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_imu_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_mavlink_interface.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_motor_model.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_multirotor_base_plugin.dylib $tmpdir/build_gazebo/
-cp $builddir/build_gazebo/librotors_gazebo_wind_plugin.dylib $tmpdir/build_gazebo/
-cp Tools/sitl_run.sh $tmpdir/Tools/
-cp posix-configs/SITL/init/ekf2/typhoon_h480 $tmpdir/posix-configs/SITL/init/ekf2/
-cp Tools/posix_lldbinit $tmpdir/Tools/
-cp Tools/posix.gdbinit $tmpdir/Tools/
-cp Tools/setup_gazebo.bash $tmpdir/Tools/
-cp Tools/sitl_gazebo/worlds/typhoon_h480.world $tmpdir/Tools/sitl_gazebo/worlds/
-cp -r Tools/sitl_gazebo/models/typhoon_h480 $tmpdir/Tools/sitl_gazebo/models
-cp ROMFS/px4fmu_common/mixers/hexa_x.main.mix $tmpdir/ROMFS/px4fmu_common/mixers/
-cp ROMFS/px4fmu_common/mixers/mount_legs.aux.mix $tmpdir/ROMFS/px4fmu_common/mixers/
 
-# Create a bash script to start the sim
-echo "#!/bin/bash -e
+cp $builddir/build_gazebo/libgazebo_geotagged_images_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/libgazebo_lidar_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/libgazebo_opticalFlow_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/libLiftDragPlugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/libmav_msgs.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_controller_interface.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_gimbal_controller_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_imu_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_mavlink_interface.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_motor_model.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_multirotor_base_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $builddir/build_gazebo/librotors_gazebo_wind_plugin.$lib_ending $tmpdir/build_gazebo/
+cp $firmwaredir/Tools/sitl_run.sh $tmpdir/Tools/
+cp $firmwaredir/posix-configs/SITL/init/ekf2/typhoon_h480 $tmpdir/posix-configs/SITL/init/ekf2/
+cp $firmwaredir/Tools/posix_lldbinit $tmpdir/Tools/
+cp $firmwaredir/Tools/posix.gdbinit $tmpdir/Tools/
+cp $firmwaredir/Tools/setup_gazebo.bash $tmpdir/Tools/
+cp $firmwaredir/Tools/sitl_gazebo/worlds/typhoon_h480.world $tmpdir/Tools/sitl_gazebo/worlds/
+cp -r $firmwaredir/Tools/sitl_gazebo/models/typhoon_h480 $tmpdir/Tools/sitl_gazebo/models
+cp $firmwaredir/ROMFS/px4fmu_common/mixers/hexa_x.main.mix $tmpdir/ROMFS/px4fmu_common/mixers/
+cp $firmwaredir/ROMFS/px4fmu_common/mixers/mount_legs.aux.mix $tmpdir/ROMFS/px4fmu_common/mixers/
 
-if [ -L ROMFS ]; then
-    rm ROMFS
-fi
-if [ -L test_data ]; then
-    rm test_data
-fi
-Tools/sitl_run.sh src/firmware/posix/px4 posix-configs/SITL/init/ekf2 none gazebo typhoon_h480 . ." > \
-    $tmpdir/typhoon_sitl.bash
+# Add bash script to start it
+cp typhoon_sitl.bash $tmpdir/typhoon_sitl.bash
 chmod +x $tmpdir/typhoon_sitl.bash
+# And add a readme
+cp README_package.md $tmpdir/README.md
 
 # copy everything into zip
 curdir=`pwd`
-(cd $tmpdir && zip -r $curdir/Yuneec-SITL-flightcode-macOS-$version.zip .)
+(cd $tmpdir && zip -r $curdir/Yuneec-SITL-Simulation-$os_string-$version.zip .)
