d9acba1e73f2aa12749e1126156bac264728d151
Julian Oes <julian@oes.ch>
2017-05-29 08:54:52 +0200

    mavlink: new MAV_CMD_IMAGE_START_CAPTURE spec
    The spec of the mavlink command MAV_CMD_IMAGE_START_CAPTURE has changed.

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -1436,9 +1436,9 @@ protected:
 				msg_cmd.target_component = MAV_COMP_ID_CAMERA;
 				msg_cmd.command = MAV_CMD_IMAGE_START_CAPTURE;
 				msg_cmd.confirmation = 0;
-				msg_cmd.param1 = 0;
-				msg_cmd.param2 = 1;
-				msg_cmd.param3 = 0;
+				msg_cmd.param1 = 0; // all cameras
+				msg_cmd.param2 = 0; // duration 0 because only taking one picture
+				msg_cmd.param3 = 1; // only take one
 				msg_cmd.param4 = NAN;
 				msg_cmd.param5 = NAN;
 				msg_cmd.param6 = NAN;
