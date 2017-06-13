c8ac8906ef87cf30f72a601dcae9689e8657c795
Julian Oes <julian@oes.ch>
2017-06-13 08:00:11 +0200

    px4_sitl_packager: fix startup script path
    The startup script path changed from ekf2 to default. This broke the
    SITL build.

--- Tools/px4_sitl_packager/typhoon_sitl.bash
+++ Tools/px4_sitl_packager/typhoon_sitl.bash
@@ -6,4 +6,4 @@ fi
 if [ -L test_data ]; then
     rm test_data
 fi
-Tools/sitl_run.sh src/firmware/posix/px4 posix-configs/SITL/init/ekf2 none gazebo typhoon_h480 . .
+Tools/sitl_run.sh src/firmware/posix/px4 posix-configs/SITL/init/default none gazebo typhoon_h480 . .
