0b12362331748e1bed44307ff7c9f3fcb814e86e
Lorenz Meier <lorenz@px4.io>
2017-07-16 22:24:26 +0200

    MAVLink: Add capture command to command queue

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -1428,24 +1428,24 @@ protected:
 
 			/* ensure that only active trigger events are sent */
 			if (trigger.timestamp > 0) {
+
 				mavlink_msg_camera_trigger_send_struct(_mavlink->get_channel(), &msg);
 
-				/* send MAV_CMD_IMAGE_START_CAPTURE */
-				mavlink_command_long_t msg_cmd;
+				vehicle_command_s cmd{};
 
-				msg_cmd.target_system = mavlink_system.sysid;
-				msg_cmd.target_component = MAV_COMP_ID_CAMERA;
-				msg_cmd.command = MAV_CMD_IMAGE_START_CAPTURE;
-				msg_cmd.confirmation = 0;
-				msg_cmd.param1 = 0; // all cameras
-				msg_cmd.param2 = 0; // duration 0 because only taking one picture
-				msg_cmd.param3 = 1; // only take one
-				msg_cmd.param4 = NAN;
-				msg_cmd.param5 = NAN;
-				msg_cmd.param6 = NAN;
-				msg_cmd.param7 = NAN;
+				cmd.target_system = mavlink_system.sysid;
+				cmd.target_component = MAV_COMP_ID_CAMERA;
+				cmd.command = MAV_CMD_IMAGE_START_CAPTURE;
+				cmd.confirmation = 0;
+				cmd.param1 = 0; // all cameras
+				cmd.param2 = 0; // duration 0 because only taking one picture
+				cmd.param3 = 1; // only take one
+				cmd.param4 = NAN;
+				cmd.param5 = NAN;
+				cmd.param6 = NAN;
+				cmd.param7 = NAN;
 
-				mavlink_msg_command_long_send_struct(_mavlink->get_channel(), &msg_cmd);
+				MavlinkCommandSender::instance().handle_vehicle_command(cmd, _mavlink->get_channel());
 			}
 		}
 	}
